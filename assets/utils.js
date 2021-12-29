e30ffa63d5b9040f2aea9051df3b358fc652c10a
Joona Yoon <joonas.yoon@gmail.com>
2021-12-29 17:34:01 +0900

    add 3d orthographic

--- assets/utils.js
+++ assets/utils.js
@@ -128,4 +128,155 @@ const Utils = {
       }
     }
   },
+  MATH: {
+    MAT3: {
+      translation: function(tx, ty) {
+        return [
+          1, 0, 0,
+          0, 1, 0,
+          tx, ty, 1
+        ];
+      },
+      rotation: function(angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+        return [
+          c,-s, 0,
+          s, c, 0,
+          0, 0, 1
+        ];
+      },
+      scaling: function(sx, sy) {
+        return [
+          sx, 0, 0,
+          0, sy, 0,
+          0, 0, 1
+        ];
+      },
+    },
+    MAT4: {
+      translation: function(tx, ty, tz) {
+        return [
+          1,  0,  0,  0,
+          0,  1,  0,  0,
+          0,  0,  1,  0,
+          tx, ty, tz, 1,
+        ];
+      },
+      rotationX: function(angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+        return [
+          1, 0, 0, 0,
+          0, c, s, 0,
+          0, -s, c, 0,
+          0, 0, 0, 1,
+        ];
+      },
+      rotationY: function(angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+        return [
+          c, 0, -s, 0,
+          0, 1, 0, 0,
+          s, 0, c, 0,
+          0, 0, 0, 1,
+        ];
+      },
+      rotationZ: function(angleInRadians) {
+        var c = Math.cos(angleInRadians);
+        var s = Math.sin(angleInRadians);
+        return [
+          c, s, 0, 0,
+          -s, c, 0, 0,
+          0, 0, 1, 0,
+          0, 0, 0, 1,
+        ];
+      },
+      scaling: function(sx, sy, sz) {
+        return [
+          sx, 0,  0,  0,
+          0, sy,  0,  0,
+          0,  0, sz,  0,
+          0,  0,  0,  1,
+        ];
+      },
+      translate: function(m, tx, ty, tz) {
+        return Utils.MATH.MAT4.multiply(m, Utils.MATH.MAT4.translation(tx, ty, tz));
+      },
+      rotateX: function(m, angleInRadians) {
+        return Utils.MATH.MAT4.multiply(m, Utils.MATH.MAT4.rotationX(angleInRadians));
+      },
+      rotateY: function(m, angleInRadians) {
+        return Utils.MATH.MAT4.multiply(m, Utils.MATH.MAT4.rotationY(angleInRadians));
+      },
+      rotateZ: function(m, angleInRadians) {
+        return Utils.MATH.MAT4.multiply(m, Utils.MATH.MAT4.rotationZ(angleInRadians));
+      },
+      scale: function(m, sx, sy, sz) {
+        return Utils.MATH.MAT4.multiply(m, Utils.MATH.MAT4.scaling(sx, sy, sz));
+      },
+      multiply: function(a, b) {
+        const b00 = b[0 * 4 + 0];
+        const b01 = b[0 * 4 + 1];
+        const b02 = b[0 * 4 + 2];
+        const b03 = b[0 * 4 + 3];
+        const b10 = b[1 * 4 + 0];
+        const b11 = b[1 * 4 + 1];
+        const b12 = b[1 * 4 + 2];
+        const b13 = b[1 * 4 + 3];
+        const b20 = b[2 * 4 + 0];
+        const b21 = b[2 * 4 + 1];
+        const b22 = b[2 * 4 + 2];
+        const b23 = b[2 * 4 + 3];
+        const b30 = b[3 * 4 + 0];
+        const b31 = b[3 * 4 + 1];
+        const b32 = b[3 * 4 + 2];
+        const b33 = b[3 * 4 + 3];
+        const a00 = a[0 * 4 + 0];
+        const a01 = a[0 * 4 + 1];
+        const a02 = a[0 * 4 + 2];
+        const a03 = a[0 * 4 + 3];
+        const a10 = a[1 * 4 + 0];
+        const a11 = a[1 * 4 + 1];
+        const a12 = a[1 * 4 + 2];
+        const a13 = a[1 * 4 + 3];
+        const a20 = a[2 * 4 + 0];
+        const a21 = a[2 * 4 + 1];
+        const a22 = a[2 * 4 + 2];
+        const a23 = a[2 * 4 + 3];
+        const a30 = a[3 * 4 + 0];
+        const a31 = a[3 * 4 + 1];
+        const a32 = a[3 * 4 + 2];
+        const a33 = a[3 * 4 + 3];
+        return [
+          b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+          b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+          b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+          b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+          b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+          b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+          b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+          b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+          b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+          b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+          b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+          b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+          b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+          b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+          b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+          b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+        ];
+      },
+      projection: function(width, height, depth) {
+        // 주의: 이 행렬은 Y축을 뒤집어 0이 위쪽이 되도록 합니다.
+        return [
+           2 / width, 0, 0, 0,
+           0, -2 / height, 0, 0,
+           0, 0, 2 / depth, 0,
+          -1, 1, 0, 1,
+        ];
+      },
+    }
+  }
 };
