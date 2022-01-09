09dd28d89661b0d28cb686a2ec1e9df85d35f870
Joona Yoon <joonas.yoon@gmail.com>
2022-01-10 00:09:58 +0900

    fix: implements perspective

--- assets/utils.js
+++ assets/utils.js
@@ -305,6 +305,16 @@ const Utils = {
           -1, 1, 0, 1,
         ];
       },
+      perspective: function(fieldOfViewInRadians, aspect, near, far) {
+        var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+        var rangeInv = 1.0 / (near - far);
+        return [
+          f / aspect, 0, 0, 0,
+          0, f, 0, 0,
+          0, 0, (near + far) * rangeInv, -1,
+          0, 0, near * far * rangeInv * 2, 0
+        ];
+      },
     }
   }
 };
